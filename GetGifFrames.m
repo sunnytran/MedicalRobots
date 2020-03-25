13d4eedfa922106b22d7fd73e25272b146195e60
ambigyuity <52841123+ambigyuity@users.noreply.github.com>
2020-03-24 22:40:28 -0500

    Update GetGifFrames.m

--- GetGifFrames.m
+++ GetGifFrames.m
@@ -1,11 +1,15 @@
 % Get images from gif and save then to the folder
-[allframedata, map] = imread('An_MRI-Compatible_Robotic_System_for_Breast_Biopsy.gif', 'frames', 'all'); 
-alldimensions = size(allframedata); 
+[allframedata, map] = imread('An_MRI-Compatible_Robotic_System_for_Breast_Biopsy.gif', 'frames', 'all');
+alldimensions = size(allframedata);
 number_of_frames = alldimensions(end);
 for i=1:number_of_frames
 im = allframedata(:,:,1,i);
 num = num2str(i);
 % Tell your image names in first ''
 nombre = strcat('im',num,'.jpg');
-imwrite(im,map,nombre)
+imwrite(im,map,nombre)  
+A=imread(nombre);
+A=rgb2gray(A);
+imshow(A);
 end
+
